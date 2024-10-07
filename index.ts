241a7d62f34b768c3809042abf29117048cca0c3
IsaacOrtga <ortga.isaac@gmail.com>
2024-10-07 22:05:44 +0200

    initial commit

---
+++ index.ts
@@ -0,0 +1,40 @@
+//Definimos las distintas interfaces que usaremos
+
+interface Trainer{
+    name: string;
+    reputation: number;
+    availablePlaces: number;
+}
+
+interface Client{
+    name: string;
+    expectation: number;
+}
+
+interface Assigned{
+    client: string;
+    trainer: string;
+    reputation: number;
+    expectation: number;
+}
+
+// Asignamos los valores a las interfaces
+const trainers: Trainer[] = [
+    {name: 'A', reputation: 4.5, availablePlaces: 1},
+    {name: 'B', reputation: 3.2, availablePlaces: 4},
+    {name: 'C', reputation: 1.2, availablePlaces: 3},
+    {name: 'D', reputation: 3.4, availablePlaces: 2},
+];
+
+const clients: Client[] = [
+    {name: 'q', expectation: 2.6},
+    {name: 'r', expectation: 3.7},
+    {name: 's', expectation: 8.5},
+    {name: 't', expectation: 9.7},
+    {name: 'u', expectation: 2.6},
+    {name: 'v', expectation: 4.7},
+    {name: 'w', expectation: 5.6},
+    {name: 'x', expectation: 3.7},
+    {name: 'y', expectation: 8.1},
+    {name: 'z', expectation: 2.5},
+]
