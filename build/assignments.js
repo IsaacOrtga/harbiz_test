5a16edfb04018cd5fc0e584a04e8e23b5247089b
IsaacOrtga <ortga.isaac@gmail.com>
2024-10-08 01:02:26 +0200

    third commit

---
+++ build/assignments.js
@@ -0,0 +1,40 @@
+// Calculamos las expectativas del cliente con respecto a la reputación del entrenador
+function calculateAssignament(client, trainer) {
+    return trainer.reputation - (client.expectation / 2);
+}
+// Asignamos a la interface de Assignations cada cliente con su entrenador
+export function assignClient(trainers, clients) {
+    // ordenamos descendentemente, de mayor a menor reputación
+    trainers.sort((a, b) => b.reputation - a.reputation);
+    clients.sort((a, b) => b.expectation - a.expectation);
+    const assignations = [];
+    for (const client of clients) {
+        // Asinamos un valor inicial de null a bestTrainer, ya que al principio no lo tenemos
+        let bestTrainer = null;
+        // Comenzamos con un ajuste negativo para asegurarnos de que el primer resultado va a ser mayor
+        let bestDifference = -1;
+        for (const trainer of trainers) {
+            if (trainer.availablePlaces > 0) {
+                // Calculamos la diferencia que hay entre las expectativas y la reputación
+                let fitDifference = calculateAssignament(client, trainer);
+                // si la nueva diferencia es mayor que la mejor que hemos tenido, entonces reasignamos el valor de bestDifference y así nos aseguramos de asignar de menor diferencia a mayor
+                if (fitDifference > bestDifference) {
+                    bestDifference = fitDifference;
+                    bestTrainer = trainer;
+                }
+            }
+        }
+        // Añadimos la nueva asignación a la interface Assignations
+        if (bestTrainer) {
+            assignations.push({
+                client: client.name,
+                trainer: bestTrainer.name,
+                fitDifference: bestDifference,
+            });
+        }
+        else {
+            console.error('Trainer is not available for ' + client.name);
+        }
+    }
+    return assignations;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
+import { Trainer, Client, Assignation } from './interfaces';
 import { assignClient } from './assignments';
 // Asignamos los valores a las interfaces
-const trainers = [
+const trainers: Trainer[] = [
     { name: 'A', reputation: 4.5, availablePlaces: 1 },
     { name: 'B', reputation: 3.2, availablePlaces: 4 },
     { name: 'C', reputation: 1.2, availablePlaces: 3 },
     { name: 'D', reputation: 3.4, availablePlaces: 2 },
 ];
-const clients = [
+
+const clients: Client[] = [
     { name: 'q', expectation: 2.6 },
     { name: 'r', expectation: 3.7 },
     { name: 's', expectation: 8.5 },
@@ -18,5 +20,7 @@ const clients = [
     { name: 'y', expectation: 8.1 },
     { name: 'z', expectation: 2.5 },
 ];
-const assignments = assignClient(trainers, clients);
+
+
+const assignments: Assignation[] = assignClient(trainers, clients);
 console.log(assignments);
